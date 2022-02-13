171e11c739ad3aa7e865411ae3be01293f958f4f
Rawston <houston_cam@msn.com>
2022-02-12 21:42:46 -0300

    comando Collectfast

--- pypro/urls.py
+++ pypro/urls.py
@@ -13,8 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import admin as admin
-import path as path
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
