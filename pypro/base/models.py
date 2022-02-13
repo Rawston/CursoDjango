e2072a5b2719690992c7465e064a245e482b865d
Rawston <houston_cam@msn.com>
2022-02-12 21:38:17 -0300

    comando Collectfast

--- pypro/base/models.py
+++ pypro/base/models.py
@@ -1,11 +1,16 @@
+from django.contrib import auth
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import PermissionDenied
 from django.contrib.auth.models import PermissionsMixin
 from django.core.mail import send_mail
 from django.db import models
+from django.db.models.manager import EmptyManager
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
+# from .validators import UnicodeUsernameValidator
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
@@ -23,7 +28,6 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email=None, password=None, **extra_fields):
