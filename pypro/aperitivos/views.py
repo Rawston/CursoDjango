8c1917bd35d62d82ee7c6ac376c34a33f98eacd2
houston_cam@msn.com <houston_cam@msn.com>
2022-07-02 01:10:11 -0300

    Video de Acordo com Slug!!

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivo: Motivaçao', 'youtube_id': 695772}
+    video = {
+        'motivacao':{'titulo': 'Video Aperitivo: Motivaçao', 'youtube_id': 695772},
+        'instalacao-windows':{'titulo': 'Instalação do Windows', 'youtube_id': 251497668},
+    }
+    video = video[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
