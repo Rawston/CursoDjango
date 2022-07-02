362ec50df6fba3de561ec24a960e38eef1cbb37b
houston_cam@msn.com <houston_cam@msn.com>
2022-07-02 00:31:19 -0300

    Bloco de Template!!

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = {'titulo': 'Video Aperitivo: Motivaçao', 'youtube_id': 695772}
+    return render(request, 'aperitivos/video.html', context={'video': video})
